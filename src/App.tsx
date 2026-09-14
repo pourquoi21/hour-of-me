58504392e6ad17082b9afc6ce42875d40946f3b5
pourquoi21 <miyaku219@naver.com>
2026-09-14 20:52:15 +0900

    feat: 오늘 기록 완료 화면 및 다시 쓰기 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { RecordView } from './views/RecordView';
 
 function App() {
   const { isLoading, hasStoredSettings, updateSettings, settings } = useSettingsStorage();
-  const { saveRecord } = useRecordStorage(); 
+  const { records, saveRecord, deleteRecord } = useRecordStorage(); 
   
   if (isLoading) {
     return <div>로딩 중...</div>
@@ -15,13 +15,47 @@ function App() {
   if (!hasStoredSettings) {
     return <OnboardingView onSaveSettings={updateSettings} />;
   }
+  
+  const todayStr = new Date().toISOString().split("T")[0];
+  const todayRecord = records.find((r) => r.date === todayStr);
 
   return (
     <div>
       메인 화면
-      <RecordView
-        onSaveRecord={saveRecord}
-        targetHour={settings.targetHour} />
+
+      {todayRecord ? (
+        <div>
+          <div
+            style={{
+              width: 48,
+              height: 48,
+              borderRadius: "15%",
+              backgroundColor: todayRecord.color,
+            }}
+            />
+            <h3>{todayRecord.emotionLabel}</h3>
+            {todayRecord.note && (
+              <p
+                style={{
+                  color: "#888",
+                  fontStyle: "italic",
+                }}
+              >
+                "{todayRecord.note}"
+              </p>
+            )}
+            <button
+              type="button"
+              onClick={() => deleteRecord(todayRecord.id)}
+            >
+              다시쓰기
+            </button>
+        </div>
+      ) : (
+        <RecordView
+          onSaveRecord={saveRecord}
+          targetHour={settings.targetHour} />
+      )}
     </div>
   );
 }
